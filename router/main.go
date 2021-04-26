f57a72b4de5e756b3c9876e039ce321a8b8c0728
denis.o <denis.o@linux.com>
2021-04-26 22:59:49 +0300

    Basic router code

--- router/main.go
+++ router/main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/app", func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprint(w, "Application application router")
-	},
-	)
-	log.Print("Up and ready")
-	http.ListenAndServe(":8080", nil)
+	log.Print("Starting router...")
+	router := Router()
+	log.Print("Starting web server")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
