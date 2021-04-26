eddf068fdfee57c1af36df4b36a378bd7f2fe28e
denis.o <denis.o@linux.com>
2021-04-26 23:12:02 +0300

    Cleanup

--- router/handler/home.go
+++ router/handler/home.go
@@ -1,4 +1,4 @@
-package main
+package handler
 
 import (
 	"fmt"
@@ -6,5 +6,5 @@ import (
 )
 
 func home(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprint(w, "Home request")
+	fmt.Fprint(w, "Home handler")
 }
--- router/main.go
+++ router/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"./handler"
 )
 
 func main() {
 	log.Print("Starting router...")
-	router := Router()
+	router := handler.Router()
 	log.Print("Starting web server")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
